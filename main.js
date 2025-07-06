ad9c343b6fbfda541bb59889239ee3e987ba60a0
ali.rabiee <a.rabiee@fidilio.com>
2025-07-06 20:35:57 +0330

    EP-251

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const swaggerConfig = require('./src/config/swagger.config')
 dotenv.config()
 
 async function main() {
     const app = express()
     const port = process.env.PORT
     require('./src/config/mongoose.config')
+    swaggerConfig(app)
 
     app.listen(port, () => console.log(`server running: http://localhost:${port}`))
 }
---
+++ src/config/swagger.config.js
@@ -0,0 +1,21 @@
+const swaggerjsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+
+function swaggerConfig(app) {
+    const swaggerDocument = swaggerjsDoc({
+        swaggerDefinition: {
+            info: {
+                title: 'divar-backend',
+                description: 'divar with experss.js APIs',
+                version: '1.0.0'
+            },
+        },
+        apis: []
+    })
+    const swagger = swaggerUi.setup(swaggerDocument, {})
+    app.use('/', swaggerUi.serve, swagger)
+}
+
+
+module.exports = swaggerConfig
